07a4dc871ee1dcbe562c1cc4125e573bd3dfa62d
Merve Ayaz <ayaz.merve1998@gmail.com>
2020-08-09 16:00:05 +0300

    Jquery and Bootstap dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppuserComponent } from './appuser/appuser.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { EdituserComponent } from './edituser/edituser.component';
+import { ViewuserComponent } from './viewuser/viewuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppuserComponent,
+    UserlistComponent,
+    EdituserComponent,
+    ViewuserComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/appuser/appuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appuser',
+  templateUrl: './appuser.component.html',
+  styleUrls: ['./appuser.component.css']
+})
+export class AppuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
