8cd6fbb034d149a3723120f166847b26f47354b6
Merve Ayaz <ayaz.merve1998@gmail.com>
2020-08-11 01:26:47 +0300

    Adding table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserlistComponent } from './userlist/userlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:UserlistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
